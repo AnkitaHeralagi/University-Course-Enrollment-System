a27edf00a3e5d4c46fbdd21bbb692c78b5c44014
AnkitaHeralagi <126943633+AnkitaHeralagi@users.noreply.github.com>
2026-03-13 20:28:23 +0530

    Updated

--- ClassroomCourse.cs
+++ ClassroomCourse.cs
@@ -1,29 +1,34 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Text;
 using static System.Net.WebRequestMethods;
 
-class ClassroomCourse : CoreCourse
+namespace University_Course_Enrollment_System
 {
-    private string hallNumber;
-    private int maxSeats;
+    internal class ClassroomCourse : CoreCourse
+    {
+        private string hallNumber;
+        private int maxSeats;
 
-    public ClassroomCourse(string title, string room, int capacity) : base(title)
-    {
-        hallNumber = room;
-        maxSeats = capacity;
-    }
-    public override void EnrollParticipant()
-    {
-        if (maxSeats > 0)
+        public ClassroomCourse(string title, string room, int capacity) : base(title)
         {
-            Console.WriteLine("\nEnrollment Approved!");
-            Console.WriteLine("Course Mode : In-Person");
-            Console.WriteLine("Course Name : " + title);
-            Console.WriteLine("Room Number : " + hallNumber);
-            Console.WriteLine("Capacity    : " + maxSeats);
+            hallNumber = room;
+            maxSeats = capacity;
         }
-        else
+        public override void EnrollParticipant()
         {
-            Console.WriteLine("Invalid capacity. It must be greater than zero.");
+            if (maxSeats > 0)
+            {
+                Console.WriteLine("\nEnrollment Approved!");
+                Console.WriteLine("Course Mode : In-Person");
+                Console.WriteLine("Course Name : " + title);
+                Console.WriteLine("Room Number : " + hallNumber);
+                Console.WriteLine("Capacity    : " + maxSeats);
+            }
+            else
+            {
+                Console.WriteLine("Invalid capacity. It must be greater than zero.");
+            }
         }
     }
 }
